b2d5cc3644f4018fbe5f3b86a669023bd2a25f89
R014ND <60377775+R014ND@users.noreply.github.com>
2022-02-21 06:28:38 +0100

    Add files via upload

---
+++ NeuralNetworkLibrary/NeuralNetworkLibrary/Command.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkLibrary
+{
+    struct training_data
+    {
+        public double[] inputs;
+        public double[] target;
+    }
+    class Command
+    {
+
+        public List<NeuralNetwork> nns = new List<NeuralNetwork>();
+        string command;
+        string[] attributes;
+        string prefix = "!";
+        
+        public Command(string prefix, string command, string[] att)
+        {
+            #region filling data arr
+            training_data[] training_Datas = new training_data[16];
+            //AND
+            training_Datas[0].inputs = new double[] { 0, 0 };
+            training_Datas[0].target = new double[] { 0 };
+
+            training_Datas[1].inputs = new double[] { 1, 0 };
+            training_Datas[1].target = new double[] { 0 };
+
+            training_Datas[2].inputs = new double[] { 1, 1 };
+            training_Datas[2].target = new double[] { 1 };
+
+            training_Datas[3].inputs = new double[] { 0, 1 };
+            training_Datas[3].target = new double[] { 0 };
+
+            //NAND
+            training_Datas[4].inputs = new double[] { 0, 0 };
+            training_Datas[4].target = new double[] { 1 };
+
+            training_Datas[5].inputs = new double[] { 1, 0 };
+            training_Datas[5].target = new double[] { 1 };
+
+            training_Datas[6].inputs = new double[] { 1, 1 };
+            training_Datas[6].target = new double[] { 0 };
+
+            training_Datas[7].inputs = new double[] { 0, 1 };
+            training_Datas[7].target = new double[] { 1 };
+
+            //OR
+            training_Datas[8].inputs = new double[] { 0, 0 };
+            training_Datas[8].target = new double[] { 0 };
+
+            training_Datas[9].inputs = new double[] { 1, 0 };
+            training_Datas[9].target = new double[] { 1 };
+
+            training_Datas[10].inputs = new double[] { 1, 1 };
+            training_Datas[10].target = new double[] { 1 };
+
+            training_Datas[11].inputs = new double[] { 0, 1 };
+            training_Datas[11].target = new double[] { 1 };
+
+            //XOR
+            training_Datas[12].inputs = new double[] { 0, 0 };
+            training_Datas[12].target = new double[] { 0 };
+
+            training_Datas[13].inputs = new double[] { 1, 0 };
+            training_Datas[13].target = new double[] { 1 };
+
+            training_Datas[14].inputs = new double[] { 1, 1 };
+            training_Datas[14].target = new double[] { 0 };
+
+            training_Datas[15].inputs = new double[] { 0, 1 };
+            training_Datas[15].target = new double[] { 1 };
+            #endregion
+
+            if (this.prefix != prefix)
+            {
+                Console.WriteLine("Command not found.");
+            }
+            switch (command)
+            {
+                case "newNN":
+                    bool exists = false;
+                    for (int i = 0; i < nns.Count; i++)
+                    {
+                        if (nns[i].name == att[0])
+                        {
+                            Console.WriteLine("Neural network under the name " + att[0] + " already exists");
+                            exists = true;
+                        }
+                    }
+                    if (!exists)
+                    {
+                        NeuralNetwork nn = new NeuralNetwork(att[0], int.Parse(att[1]), int.Parse(att[2]), int.Parse(att[3]));
+                        this.nns.Add(nn);
+                        Console.WriteLine("New neural network " + nn.name + " has been created");
+                    }
+                    break;
+                case "list":
+                    for (int i = 0; i < this.nns.Count; i++)
+                    {
+                        Console.WriteLine(nns[i].name + " (" + nns[i].input_nodes + ", " + nns[i].hidden_nodes + ", " + nns[i].output_nodes + ")");
+                    }
+                    break;
+                case "trainNN":
+                    int index = -1;
+                    for (int i = 0; i < nns.Count; i++)
+                    {
+                        if (nns[i].name == att[0])
+                        {
+                            index = i;
+                        }
+                    }
+                    switch (att[1])
+                    {
+                        case "AND":
+                            for (int i = 0; i < int.Parse(att[2]); i++)
+                            {
+                                Random r = new Random();
+                                int rand = r.Next(0, 4);
+                                nns[index].train(training_Datas[rand].inputs, training_Datas[rand].target);
+                            }
+                            break;
+                        case "NAND":
+                            for (int i = 0; i < int.Parse(att[2]); i++)
+                            {
+                                Random r = new Random();
+                                int rand = r.Next(4, 8);
+                                nns[index].train(training_Datas[rand].inputs, training_Datas[rand].target);
+                            }
+                            break;
+                        case "OR":
+                            for (int i = 0; i < int.Parse(att[2]); i++)
+                            {
+                                Random r = new Random();
+                                int rand = r.Next(8, 12);
+                                nns[index].train(training_Datas[rand].inputs, training_Datas[rand].target);
+                            }
+                            break;
+                        case "XOR":
+                            for (int i = 0; i < int.Parse(att[2]); i++)
+                            {
+                                Random r = new Random();
+                                int rand = r.Next(12, 16);
+                                nns[index].train(training_Datas[rand].inputs, training_Datas[rand].target);
+                            }
+                            break;
+                    }
+                    Console.WriteLine("Training done on " + att[0] + " network");
+                    break;
+                default:
+                    Console.WriteLine("Command not found");
+                    break;
+            }
+        }
+    }
+}
---
+++ NeuralNetworkLibrary/NeuralNetworkLibrary/Matrix.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkLibrary
+{   
+    class Matrix
+    {
+        public double[,] values;
+        public int rows, cols;
+
+        //constructor
+        public Matrix(int rows, int cols, bool isEmpty = false)
+        {
+            values = new double[rows, cols];
+            this.rows = rows;
+            this.cols = cols;
+            if (!isEmpty)
+            {
+                for (int i = 0; i < this.rows; i++)
+                {
+                    for (int j = 0; j < this.cols; j++)
+                    {
+                        this.values[i, j] = -1;
+                    }
+                }
+            }
+        }
+
+        //basic operations
+
+        //fill the matrix with random numbers between 0.0 and 1.0
+        public void randomize()
+        {
+            Random r = new Random();
+            //order does not matter but this does it in TOP -> BOTTOM first and then UP->RIGHT order (simply change cycles to do it reversed)
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] = r.NextDouble();
+                }
+            }
+        }
+        public void print()
+        {
+            string line;
+            for (int i = 0; i < this.rows; i++)
+            {
+                line = "";
+                for (int j = 0; j < this.cols; j++)
+                {
+                    line += this.values[i, j] + " ";
+                }
+                Console.WriteLine(line);
+            }
+        }
+        //set all values in matrix to a given parameter
+        public void setValues(double v)
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] = v;
+                }
+            }
+        }
+        //derive
+        public Matrix dSigmoid()
+        {
+            Matrix output = new Matrix(this.rows, this.cols);
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    output.values[i, j] = this.values[i, j] * (1 - this.values[i, j]);
+                }
+            }
+            return output;
+        }
+        //activation function
+        public void activate()
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] = 1 / (1 + Math.Pow(Math.E, -this.values[i, j]));
+                }
+            }
+        }
+        //transpose
+        public Matrix transpose()
+        {
+            Matrix output = new Matrix(this.cols, this.rows);
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    output.values[j, i] = this.values[i, j];
+                }
+            }
+            return output;
+        }
+
+        //scalar operations--------------------------------------------------
+
+        //add parameter s to every value
+        public void addScalar(double s)
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] += s;
+                }
+            }
+        }
+        //subtraction
+        public void subtractScalar(double s)
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] -= s;
+                }
+            }
+        }
+        //multipliy all values by parameter s
+        public void multiplyScalar(double s)
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] *= s;
+                }
+            }
+        }
+        //division, since values are stored in a double array i can do this just for fun
+        public void divideScalar(double s)
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] /= s;
+                }
+            }
+        }
+
+        //elementwise operations---------------------------------------------
+        //operates with elements on the same coordinates (!!!requires 2 matrices with the same dimensions!!!)
+        
+        //elementwise addition
+        public void addMatrix(Matrix a)
+        {
+            //custom exception for size diff in matrices
+            if (this.rows != a.rows || this.cols != a.cols)
+            {
+                throw new ArgumentException("Matrices should have the same number of rows and the same number of columns.");
+            }
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] += a.values[i, j]; 
+                }
+            }
+        }
+        //elementwise multiplication
+        public void multiplyMatrix(Matrix a)
+        {
+            //custom exception for size diff in matrices
+            if (this.rows != a.rows || this.cols != a.cols)
+            {
+                throw new ArgumentException("Matrices should have the same number of rows and the same number of columns.");
+            }
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.values[i, j] *= a.values[i, j];
+                }
+            }
+        }
+        //same functions but they return a new matrix as the result of the EW operations
+        public Matrix addEW(Matrix a)
+        {
+            Matrix output = new Matrix(a.rows, a.cols);
+            //custom exception for size diff in matrices
+            if (this.rows != a.rows || this.cols != a.cols)
+            {
+                throw new ArgumentException("Matrices should have the same number of rows and the same number of columns.");
+            }
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    output.values[i, j] = this.values[i, j] + a.values[i, j];
+                }
+            }
+
+            return output;
+        }
+        public Matrix multiplyEW(Matrix a)
+        {
+            Matrix output = new Matrix(a.rows, a.cols);
+            //custom exception for size diff in matrices
+            if (this.rows != a.rows || this.cols != a.cols)
+            {
+                throw new ArgumentException("Matrices should have the same number of rows and the same number of columns.");
+            }
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    output.values[i, j] = this.values[i, j] * a.values[i, j];
+                }
+            }
+
+            return output;
+        }
+        public Matrix subtractMatrix(Matrix a)
+        {
+            Matrix output = new Matrix(this.rows, this.cols);
+            if (this.rows != a.rows || this.cols != a.cols)
+            {
+                throw new ArgumentException("Matrices should have the same number of rows and the same number of columns.");
+            }
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    output.values[i, j] = this.values[i, j] - a.values[i, j];
+                }
+            }
+            return output;
+        }
+
+        //matrix multiplication---------------------------------------------
+        //complete mess
+        //For matrix multiplication, the number of columns in the first matrix must be equal
+        //to the number of rows in the second matrix
+        public Matrix dotProduct(Matrix a)
+        {
+            Matrix output = new Matrix(this.rows, a.cols);
+            if (this.cols != a.rows)
+            {
+                throw new ArgumentException("To calculate the dot product you must have matrices with opposite length in dimensions");
+            }
+
+            for (int i = 0; i < output.rows; i++)
+            {
+                for (int j = 0; j < output.cols; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < this.cols; k++)
+                    {
+                        sum += this.values[i, k] * a.values[k, j];
+                    }
+                    output.values[i, j] = sum;
+                }
+            }
+
+            return output;
+        }
+    }
+}
